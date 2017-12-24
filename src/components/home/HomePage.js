f64c7136d051feb66f8b78453290588146673ce9
omadoyeabraham <omadoyeabraham@gmail.com>
2017-12-24 21:37:21 +0100

    added the link to the about page on the homepage

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -11,6 +11,7 @@ class HomePage extends React.Component {
       <div className="jumbotron">
         <h1>Pluralsight Administration</h1>
         <p>React, Redux and React Router in ES6 for ultra responsive web apps</p>
+        <Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
       </div>
     )
   }
