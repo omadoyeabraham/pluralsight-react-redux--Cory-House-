6d8c3edab781139ffd6e02ae2106ab3079d5299a
omadoyeabraham <omadoyeabraham@gmail.com>
2017-12-24 21:34:30 +0100

    initial commit with most of the development environment already set up

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router';
+
+class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>Pluralsight Administration</h1>
+        <p>React, Redux and React Router in ES6 for ultra responsive web apps</p>
+      </div>
+    )
+  }
+}
