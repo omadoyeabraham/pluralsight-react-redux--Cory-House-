0805a31dfdbe8aa96dfb3248bf16713e33834049
omadoyeabraham <omadoyeabraham@gmail.com>
2017-12-25 09:19:08 +0100

    added the header component

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -13,6 +13,8 @@ class HomePage extends React.Component {
         <p>React, Redux and React Router in ES6 for ultra responsive web apps</p>
         <Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
       </div>
-    )
+    );
   }
 }
+
+export default HomePage;
